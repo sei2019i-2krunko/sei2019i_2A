fae4865e84e69b0cf2f7984c7b6c7eb7b8e5fb00
Jhon Acosta <jfacostamu@unal.edu.co>
2019-07-01 18:27:58 -0500

    modificado loginController

--- code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/businessLogic/LoginController.java
+++ code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/businessLogic/LoginController.java
@@ -2,17 +2,20 @@ package co.edu.unal.krunko.sitespins.businessLogic;
 
 public class LoginController {
 
+    //User usuario;
+    //Instancia de lo que maneja firebase
+
     public String isAuthenticated(String personal_id, String password){
         if(!personal_id.isEmpty()){
             if(!password.isEmpty()){
                 int id = Integer.parseInt(personal_id);
                 int pass = Integer.parseInt(password);
 
-                // Obtener el usuario con id
-                // Handle
+                //usuario = Obtener el usuario con id haciendo uso de la instacia de firebase
+
                 if (true/*existe el ususario*/){
                     if(true/*si coicide la contraseña*/){
-                        return "Connection Successful";
+                        return "Successful Connection";
                     }
                     return "Incorrect Password";
                 }
