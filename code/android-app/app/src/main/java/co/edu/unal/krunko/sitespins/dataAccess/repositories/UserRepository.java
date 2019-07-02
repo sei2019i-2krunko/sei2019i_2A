618b614f0434ef5fb70266537b3bc47c79aa7f16
daruiz31 <d4ruiz31@gmail.com>
2019-07-02 17:12:12 -0500

    User's method createUserEmailAndPassword was created.
    Co-Authored-By: HASPIMMA <hpintom@unal.edu.co>

--- code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/dataAccess/repositories/UserRepository.java
+++ code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/dataAccess/repositories/UserRepository.java
@@ -1,16 +1,44 @@
 package co.edu.unal.krunko.sitespins.dataAccess.repositories;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executor;
+
+import co.edu.unal.krunko.sitespins.dataAccess.models.User;
 
 
 public class UserRepository {
 
-	private FirebaseAuth auth;
+    private FirebaseAuth auth;
+    private User user;
 
-	public UserRepository() {
-		this.auth = FirebaseAuth.getInstance();
-	}
+    public UserRepository() {
+        this.auth = FirebaseAuth.getInstance();
+    }
 
-	//TODO: implement the creation of the user with email and password
+    //TODO: implement the creation of the user with email and password
+    public User createUserWithEmailAndPassword(String _email, String _password) throws Exception {
+        final Exception[] exception = {null};
+        auth.createUserWithEmailAndPassword(_email, _password).addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    user = User.fromFirebaseUser(auth.getCurrentUser());
+                } else {
+                    user = null;
+                    exception[0] = task.getException();
+                }
+            }
+        });
 
+        if (exception[0] == null) {
+            throw exception[0];
+        }
+        return user;
+    }
 }
