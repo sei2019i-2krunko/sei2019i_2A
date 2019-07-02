61e39c7fe4f1f2e5c46401a4976d6ba5f33e0472
HASPIMA <hpintom@unal.edu.co>
2019-07-01 21:02:31 -0500

    Firebase function for creating user document was created.
    In a new user creation will be triggered the function create_user_document which will create a new document with two fields:
    · uid: user id.
    · admin: by default is going to be false.

---
+++ code/firebase-functions/functions/src/index.ts
@@ -0,0 +1,41 @@
+import * as functions from 'firebase-functions'
+import admin = require('firebase-admin')
+
+admin.initializeApp(functions.config().firebase)
+
+const db = admin.firestore()
+
+// this function creates a document when an user has been created
+// this allow us to take control of the user in firestore
+exports.create_user_document = functions.auth.user().onCreate((user, context) => {
+	if (user.uid) {
+
+		//user document
+		const u_doc = '/users/' + user.uid
+
+		console.log('[Create user document] Function will be affecting', user.uid)
+
+		console.log('[Create user document] Accessing to:', u_doc)
+		const docRef = db.collection('users').doc(user.uid)
+
+		console.log('[Create user document] Setting information up in the document:', u_doc)
+
+		const default_data = {
+            uid: user.uid,
+            admin: false,
+		}
+
+		return docRef.set(default_data).catch((error) => {
+			console.error(
+				'[Create user document] There was an error at trying to create the document',
+				u_doc,
+				'\nWith error:', error
+			)
+		})
+
+	}
+
+	console.error('[Create user document] Invalid user id', user.uid)
+	return false
+})
+
