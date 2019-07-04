d9f2a5d24517e26d65ccddde588f5438b7a23405
nmolanob <nmolanob@unal.edu.co>
2019-07-03 22:40:06 -0500

    Sign up activity was prepared to work with the controller. · New string was added to resources.

--- code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/presentation/SignupActivity.java
+++ code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/presentation/SignupActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,58 +14,74 @@ import co.edu.unal.krunko.sitespins.R;
 
 public class SignupActivity extends AppCompatActivity {
 
-    EditText _nameText;
-    EditText _emailText;
-    EditText _passwordText;
+	EditText _nameText;
+	EditText _emailText;
+	EditText _passwordText;
 
-    EditText _reEnterPasswordText;
-    Button _signupButton;
-    TextView _loginLink;
+	EditText _reEnterPasswordText;
+	Button _signupButton;
+	TextView _loginLink;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signup);
 
-        initViews();
+		initViews();
 
-        _loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finalización del Activity de Sign-Up y regreso al Activity de Login
-                Intent backLogin = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(backLogin);
-                finish();
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
-            }
-        });
+		_loginLink.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Finalización del Activity de Sign-Up y regreso al Activity de Login
+				Intent backLogin = new Intent(getApplicationContext(), LoginActivity.class);
+				startActivity(backLogin);
+				finish();
+				overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+			}
+		});
 
-        _signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO: Login
-            }
+		final String password = getResources().getString(R.string.verify_password);
 
-        });
+		_signupButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (passwordsMatch()) {
+					createAccount(
+							_nameText.getText().toString(),
+							_emailText.getText().toString(),
+							_passwordText.getText().toString()
+					);
+				} else {
+					Toast.makeText(
+							getApplicationContext(),
+							password,
+							Toast.LENGTH_SHORT).show();
+				}
+			}
 
-        // TODO: Completar el Activity <:
-
-    }
-
-    private boolean PasswordsMatch(String _pass, String _passConfirmation) {
-        return _pass.equals(_passConfirmation);
-
-    }
+		});
 
 
-    private void initViews() {
-        // Inicialización de Botones y Campos de Texto (desde Layout)
-        _nameText = findViewById(R.id.input_name);
-        _emailText = findViewById(R.id.input_email);
-        _passwordText = findViewById(R.id.input_password);
-        _reEnterPasswordText = findViewById(R.id.input_reEnterPassword);
-        _signupButton = findViewById(R.id.btn_signup);
-        _loginLink = findViewById(R.id.link_login);
-    }
+	}
+
+	private boolean passwordsMatch() {
+		return _passwordText.getText().toString().equals(_reEnterPasswordText.getText().toString());
+
+	}
+
+	private void createAccount(String name, String email, String password) {
+		// TODO: call the controller
+	}
+
+
+	private void initViews() {
+		// Inicialización de Botones y Campos de Texto (desde Layout)
+		this._nameText = findViewById(R.id.input_name);
+		this._emailText = findViewById(R.id.input_email);
+		this._passwordText = findViewById(R.id.input_password);
+		this._reEnterPasswordText = findViewById(R.id.input_reEnterPassword);
+		this._signupButton = findViewById(R.id.btn_signup);
+		this._loginLink = findViewById(R.id.link_login);
+	}
 
 }
