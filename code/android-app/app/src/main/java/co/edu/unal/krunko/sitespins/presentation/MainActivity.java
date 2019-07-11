5ea7d1ceabd6fa1a578a538493294a9e77405e2e
daruiz31 <d4ruiz31@gmail.com>
2019-07-11 10:36:18 -0500

    User's id is display in Main Activity's welcome message.

--- code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/presentation/MainActivity.java
+++ code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/presentation/MainActivity.java
@@ -1,16 +1,20 @@
 package co.edu.unal.krunko.sitespins.presentation;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import co.edu.unal.krunko.sitespins.R;
 
 public class MainActivity extends AppCompatActivity {
 
-	Button _mapsButton;
+	private Button _mapsButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		this._mapsButton = findViewById(R.id.btn_maps);
+		TextView _welcome = findViewById(R.id.welcome_text);
+
+		if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+			_welcome.append(" " + FirebaseAuth.getInstance().getUid());
+		}
 
 		this._mapsButton.setOnClickListener(new View.OnClickListener() {
 			@Override
