46ad4a0b2b9aeb4be297cd6903da7554ff8095f9
HASPIMA <hpintom@unal.edu.co>
2019-07-17 19:43:02 -0500

    Pin's model was updated.
    autonum field was refactored to autoId (it is a String).

--- code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/dataAccess/models/Pin.java
+++ code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/dataAccess/models/Pin.java
@@ -3,14 +3,14 @@ package co.edu.unal.krunko.sitespins.dataAccess.models;
 public class Pin {
 
 	private String uid;
-	private int autoNum;
+	private String autoId;
 	private float latitude;
 	private float longitude;
 	private boolean visited;
 
-	public Pin(String uid, int autoNum, float latitude, float longitude, boolean visited) {
+	public Pin(String uid, String autoId, float latitude, float longitude, boolean visited) {
 		this.uid = uid;
-		this.autoNum = autoNum;
+		this.autoId = autoId;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.visited = visited;
@@ -24,12 +24,12 @@ public class Pin {
 		this.uid = uid;
 	}
 
-	public int getAutoNum() {
-		return autoNum;
+	public String getAutoId() {
+		return autoId;
 	}
 
-	public void setAutoNum(int autoNum) {
-		this.autoNum = autoNum;
+	public void setAutoId(String autoId) {
+		this.autoId = autoId;
 	}
 
 	public float getLatitude() {
