c8dadb39ad97ded2d4f673c40d4748b9680cc580
HASPIMA <hpintom@unal.edu.co>
2019-07-19 18:02:19 -0500

    Pin's repository has javadocs for each method.

--- code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/dataAccess/repositories/PinRepository.java
+++ code/android-app/app/src/main/java/co/edu/unal/krunko/sitespins/dataAccess/repositories/PinRepository.java
@@ -1,32 +1,84 @@
 package co.edu.unal.krunko.sitespins.dataAccess.repositories;
 
-import android.app.Activity;
-
 import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.functions.FirebaseFunctions;
 
-import co.edu.unal.krunko.sitespins.dataAccess.models.Pin;
+import co.edu.unal.krunko.sitespins.dataAccess.models.PinUser;
 
 public class PinRepository {
 
-	private Activity activity;
+	FirebaseFunctions functions;
 
-	public PinRepository(Activity activity) {
-		this.activity = activity;
+	public PinRepository() {
+		this.functions = FirebaseFunctions.getInstance();
 	}
 
-	public Pin createNewPin(GeoPoint point) {
+	/**
+	 * This method only works for non-admin users.
+	 *
+	 * @param point Pin's location.
+	 * @return A Pin instance with the parameters given with its id in Firebase.
+	 */
+	public PinUser createNewPin(GeoPoint point) {
 		return null;
 	}
 
-	public Pin createNewPin(String name, GeoPoint point) {
+	/**
+	 * This method only works for non-admin users.
+	 *
+	 * @param name  Pin's name.
+	 * @param point Pin's location.
+	 * @return A Pin instance with the parameters given with its id in Firebase.
+	 */
+	public PinUser createNewPin(String name, GeoPoint point) {
 		return null;
 	}
 
-	public Pin createNewPin(double latitude, double longitude) {
+	/**
+	 * This method only works for non-admin users.
+	 *
+	 * @param latitude  Pin's latitude coordinate.
+	 * @param longitude Pin's longitude coordinate.
+	 * @return A Pin instance with the parameters given with its id in Firebase.
+	 */
+	public PinUser createNewPin(double latitude, double longitude) {
 		return null;
 	}
 
-	public Pin createNewPin(String name, double latitude, double longitude) {
+	/**
+	 * This method only works for non-admin users.
+	 *
+	 * @param name      Pin's name.
+	 * @param latitude  Pin's latitude coordinate.
+	 * @param longitude Pin's longitude coordinate.
+	 * @return A Pin instance with the parameters given with its id in Firebase.
+	 */
+	public PinUser createNewPin(String name, double latitude, double longitude) {
+		return null;
+	}
+
+	/**
+	 * This method only works for non-admin users.
+	 *
+	 * @param name    Pin's name.
+	 * @param point   Pin's location.
+	 * @param comment Pin's comment.
+	 * @return A Pin instance with the parameters given with its id in Firebase.
+	 */
+	public PinUser createNewPin(String name, GeoPoint point, String comment) {
+		return null;
+	}
+
+	/**
+	 * This method only works for non-admin users.
+	 *
+	 * @param name      Pin's name.
+	 * @param latitude  Pin's latitude coordinate.
+	 * @param longitude Pin's longitude coordinate.
+	 * @param comment   Pin's comment.
+	 * @return A Pin instance with the parameters given with its id in Firebase.
+	 */
+	public PinUser createNewPin(String name, double latitude, double longitude, String comment) {
 		return null;
 	}
 
